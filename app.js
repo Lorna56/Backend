5d8f19f0bb5830d82cc4f56c03d5ed4363324fa5
Lorna56 <lornanaula0042@gmail.com>
2024-07-24 14:57:42 -0500

    FORM

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 
 const app = express();
 const port = 8080
@@ -13,6 +13,19 @@ app.get('/', (req, res) =>{
 app.listen(port,()=>{
 console.log(`server runing on port ${port}`)
 })
+
+//CONNECT TO THE DATABASE
+mongoose.connect("mongodb+srv://CodeQueen:Lornajesty@cluster0.woggvkl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    // {useNewUrlParser:true}
+    // parsing error 
+    
+).then(
+    ()=>{console.log('Connected to the DB')}
+
+).catch(err=>{
+    console.log('Error connecting to the DB', err)
+})
+
 
 // function student (req, res){
 //console.log(req+res)
